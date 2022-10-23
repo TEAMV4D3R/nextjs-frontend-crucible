94f4a200c1d8544646866ff3221b9700c39fcba3
Jae <86385803+Jaimierl@users.noreply.github.com>
2022-10-23 14:10:28 -0400

    Update header.js

--- crucible/components/header.js
+++ crucible/components/header.js
@@ -25,6 +25,21 @@ export const Header = ({ user, logout }) => {
                     isOpen={isOpen}
                     toggleDrawer={toggleDrawer}
                 />
+                        
+//              <div className="wrg-toggle">
+//                     <div>
+//                     </div>
+//                     <div className="wrg-toggle-container">
+//                         <div className="wrg-toggle-check">
+//                             <span>🌜</span>
+//                         </div>
+//                         <div className="wrg-toggle-uncheck">
+//                             <span>🌞</span>
+//                         </div>
+//                     </div>
+//                     <div className="wrg-toggle-circle"></div>
+//                     <input className="wrg-toggle-input" type="checkbox" aria-label="Toggle Button" />
+//                 </div>            
             </div>
         </header>
     )
