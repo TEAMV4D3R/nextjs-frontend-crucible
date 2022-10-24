295b28cb0c2f49e65abb3d83f7bbfea089c77e37
JJ Escandor <jj.p.escandor@gmail.com>
2022-10-23 20:16:28 -0400

    test

--- crucible/components/jobsearch.js
+++ crucible/components/jobsearch.js
@@ -1,9 +1,7 @@
 import useResourceTwo from "../hooks/useResourcetwo";
 import { useState } from "react";
 
-const JobSearch = ({ user, onLogin }) => {
-
-
+const JobSearch = () => {
     const { resources2, createResource2, deleteResource2 } = useResourceTwo();
     console.log(resources2)
 
@@ -22,7 +20,7 @@ const JobSearch = ({ user, onLogin }) => {
     }
 
     const [searchResult, setSearchResult] = useState([]);
-    function submitHandler(event) {
+    const submitHandler = (event) => {
         event.preventDefault();
         const newSearch = {
             keyword: event.target.keyword.value,
@@ -34,21 +32,23 @@ const JobSearch = ({ user, onLogin }) => {
 
     return (
         <div>
-            <div className=" flex h-5/6">
+            <h1>JobSearch</h1>
+            {/* <div className=" flex h-5/6">
                 <form onSubmit={submitHandler} className="flex flex-col mx-auto w-4/6 items-center">
                     <input placeholder="job" name="keyword" className="my-20 w-3/5 h-10 bg-emerald-200 rounded" />
                     <input name="location" placeholder="location" className="w-3/5 h-10 bg-emerald-200 rounded" />
                     <button className="m-5 rounded shadow-xl bg-emerald-500 px-10 py-5 ">Search</button>
                 </form>
             </div>
-            {searchResult.map((el, idx) => {
-                return <div key={idx} className="m-5">
-                    <h6>position:{el?.position}</h6>
-                    <h6>location:{el?.location}</h6>
-                    <h6>company:{el?.company}</h6>
-                    <h6>url:{el?.url}</h6>
-                </div>
-            })}
+            {searchResult.length > 0 &&
+                searchResult.map((el, idx) => {
+                    return <div key={idx} className="m-5">
+                        <h6>position:{el?.position}</h6>
+                        <h6>location:{el?.location}</h6>
+                        <h6>company:{el?.company}</h6>
+                        <h6>url:{el?.url}</h6>
+                    </div>
+                })} */}
         </div>
 
     )
