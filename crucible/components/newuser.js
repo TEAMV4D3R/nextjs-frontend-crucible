5fb1c2551256094c9ce73eb3e6ee6130d0eab37a
Jae Loney <jaimieloney@gmail.com>
2022-10-25 01:39:52 +0000

    Merge conflicts resolved

--- crucible/components/newuser.js
+++ crucible/components/newuser.js
@@ -15,7 +15,12 @@ const NewUser = ({ onCreate }) => {
     // Need to send this information to the user database.
 
     return (
+        
         <div className="bg-color-main dark:bg-color-main-dark h-[calc(100vh-10em)] dark:text-color-bright-dark">
+            <Modal
+        isOpen={props.modalIsOpen}
+        ariaHideApp={false}
+    ></Modal>
             <h1 className="flex flex-col mx-auto w-4/6 items-center">New User Sign Up</h1>
             <div className=" flex h-5/6 text-white">
                 <form onSubmit={submitHandler} className="flex flex-col mx-auto w-4/6 items-center text-black">
@@ -24,10 +29,14 @@ const NewUser = ({ onCreate }) => {
                     <input type="password" name="password" placeholder="Password" className="w-3/5 h-10 bg-color-contrast dark:bg-color-contrast-dark rounded" />
                     <p>Use your new username and password to log in on the next page!</p>
                     <button className="m-5 rounded shadow-xl bg-color-highlight dark:bg-color-highlight-dark
-                                px-8 py-5 ">Sign Up</button>
+                                px-8 py-5 "onClick={() => {
+                props.setIsModalOpen(false)
+            }}>Sign Up</button>
+                                
                 </form>
             </div>
         </div>
+        
     );
 }
 
