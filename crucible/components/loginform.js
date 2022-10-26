4e68c9851a23cf9e06cc148534b097a74d211e0e
JJ Escandor <89546590+jjescandor@users.noreply.github.com>
2022-10-26 11:40:27 -0400

    Update loginform.js

--- crucible/components/loginform.js
+++ crucible/components/loginform.js
@@ -27,7 +27,7 @@ const LoginForm = ({ onLogin }) => {
             </header>
             <div className="bg-color-main dark:bg-color-main-dark dark:text-color-bright-dark h-screen">
                 <h1 className="flex flex-col mx-auto w-4/6 items-center text-color-shadow-dark">Welcome to Crucible</h1>
-                <div className=" flex flex-col h-1/2 w-1/3 mx-auto text-white bg-slate-200 rounded shadow-2xl dark:bg-color-contrast-dark">
+                <div className=" flex flex-col h-1/2 w-1/3 mx-auto text-white bg-slate-200 rounded shadow-2xl dark:bg-color-contrast-dark overflow-y-auto">
                     <h2 className="mt-10 text-2xl text-center text-slate-800">Login to your account</h2>
                     <form onSubmit={submitHandler} className="flex flex-col mx-auto w-full items-center text-black">
                         <div className="flex items-center bg-color-contrast h-12 w-full mt-10 w-4/5 rounded">
