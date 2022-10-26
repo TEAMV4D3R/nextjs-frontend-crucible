7b40f60f247daa767f733c7e0fd26bd16d1545a4
JJ Escandor <89546590+jjescandor@users.noreply.github.com>
2022-10-26 12:09:06 -0400

    Update loginform.js

--- crucible/components/loginform.js
+++ crucible/components/loginform.js
@@ -19,7 +19,7 @@ const LoginForm = ({ onLogin }) => {
     // Need to redirect to new-user form for signup button on-click
 
     return (
-        <main  className="bg-color-main dark:bg-color-main-dark dark:text-color-bright-dark h-screen>
+        <div className="bg-color-main dark:bg-color-main-dark dark:text-color-bright-dark h-screen>
             <header className='bg-color-shadow dark:bg-color-shadow-dark flex flex-row content-center justify-between'>
                 <div className="flex">
                     <h1 className="my-auto text-3xl m-5">Crucible</h1>
