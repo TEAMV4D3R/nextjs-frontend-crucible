9ea736e430607a60a28e7a077f2d1d88fbfa9d87
Jae <86385803+Jaimierl@users.noreply.github.com>
2022-10-27 01:29:46 -0400

    Update aboutthedevs.js

--- crucible/components/aboutthedevs.js
+++ crucible/components/aboutthedevs.js
@@ -26,7 +26,7 @@ const AboutTheDevs = () => {
                             <a href="https://www.linkedin.com/in/jae-loney/"><AiFillLinkedin className="m-2 text-3xl" /></a>
                             <a href="https://github.com/Jaimierl"><AiFillGithub className="m-2 text-3xl text-3xl" /></a>
                         </div>
-                        <p className="text-justify p-3">Jae is a Software Developer with experience in Project Management and Accounts Management who loves learning, solving problems, and organizing teams. Jae enjoys working with Javascript in both front-end and back-end applications and hopes to use their skills to bring websites to life with people who are working to change the world for the better. </p>
+                        <p className="text-justify p-3">Jae is a Software Developer with experience in Project Management and Accounts Management who loves writing, solving problems, and organizing teams. Jae enjoys working with Python and Javascript in front-end and back-end applications and hopes to use their skills to bring websites to life with people who are working to change the world for the better. </p>
                     </div>
                     <div className="p-3 w-5/12">
                         <div className="rounded drop-shadow-2xl  w-64 h-64 mx-auto flex justify-center shadow-lg">
@@ -36,7 +36,7 @@ const AboutTheDevs = () => {
                             <a href="https://www.linkedin.com/in/jacobamsbury/"><AiFillLinkedin className="m-2 text-3xl" /></a>
                             <a href="https://github.com/KajeTheCat"><AiFillGithub className="m-2 text-3xl" /></a>
                         </div>
-                        <p className="text-justify p-3">Jacob is a Software Engineer, with experience in troubleshooting and finding bugs. He aims to cultivate his skills here at Code Fellows, and utilize them to better his abilities and develop video games in his near future. He is a very customer-oriented developer and listens to his clients needs.</p>
+                        <p className="text-justify p-3">Jacob is a Software Engineer, with experience in troubleshooting and finding bugs. He has cultivated his skills at Code Fellows, and will utilize them to and develop video games in the near future. He is a very customer-oriented developer and listens to his clients' needs.</p>
                     </div>
                 </div>
                 <div className="flex flex-row justify-evenly items-center h-5/6 ">
@@ -58,7 +58,7 @@ const AboutTheDevs = () => {
                             <a href="https://www.linkedin.com/in/pedrogperezalvarez/"><AiFillLinkedin className="m-2 text-3xl" /></a>
                             <a href="https://github.com/PGPere"><AiFillGithub className="m-2 text-3xl" /></a>
                         </div>
-                        <p className="text-justify p-3">Pedro have over 15+ years of work experience through various finance roles in corporations like Microsoft and Amazon.  Most recently I pivot into a fintech product role at Expedia.   As a software developer in fintech I plan to develop innovative financial  solutions for customer pain points that have been ignored by commercial banks. As a team member, my style is direct and collaborative which leads to building a better product or service together with others.</p>
+                        <p className="text-justify p-3">Pedro has over 15 years of work experience through various finance roles in corporations like Microsoft and Amazon.  Most recently he pivoted into a fintech product role at Expedia.   As a software developer in fintech he plans to develop innovative financial  solutions for customer pain points that have been ignored by commercial banks. As a team member, his style is direct and collaborative which leads to building a better products or service together with others.</p>
                     </div>
                 </div>
             </main>
